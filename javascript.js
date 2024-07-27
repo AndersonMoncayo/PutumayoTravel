451378fca0daca4cdcc423b2bbe949c174aa77d8
AndersonMoncayo <48973609+AndersonMoncayo@users.noreply.github.com>
2024-07-27 16:58:13 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+<script>
+    window.addEventListener('scroll', function() {
+        const container = document.querySelector('.container');
+        if (window.scrollY > 50) {
+            container.classList.add('scrolled');
+        } else {
+            container.classList.remove('scrolled');
+        }
+    });
+</script>

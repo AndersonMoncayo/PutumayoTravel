6b88e294eecdc8ccd2189f28ada8708deef45adf
AndersonMoncayo <48973609+AndersonMoncayo@users.noreply.github.com>
2024-07-27 17:05:02 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
-<script>
-    window.addEventListener('scroll', function() {
-        const container = document.querySelector('.container');
-        if (window.scrollY > 50) {
-            container.classList.add('scrolled');
-        } else {
-            container.classList.remove('scrolled');
-        }
-    });
-</script>
+window.addEventListener('scroll', function() {
+    const logo = document.querySelector('.logo');
+    if (window.scrollY > 50) {
+        logo.classList.add('scrolled');
+    } else {
+        logo.classList.remove('scrolled');
+    }
+});

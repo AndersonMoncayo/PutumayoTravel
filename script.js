5d7135915bf88eec36b7d6174a39b5bca21baed2
AndersonMoncayo <48973609+AndersonMoncayo@users.noreply.github.com>
2024-07-31 19:13:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
-document.getElementById('hamburger').addEventListener('click', function () {
-    document.getElementById('nav-menu').classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function () {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+
+    hamburger.addEventListener('click', function () {
+        navMenu.classList.toggle('active');
+    });
 });

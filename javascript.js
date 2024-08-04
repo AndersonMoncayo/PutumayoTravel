71d652e898dc5028305e178a9196a373e7c905c3
AndersonMoncayo <48973609+AndersonMoncayo@users.noreply.github.com>
2024-08-03 19:15:43 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,12 +1,16 @@
-// scripts.js
-
 document.addEventListener('DOMContentLoaded', function() {
     const navLinks = document.querySelectorAll('nav ul li a');
     navLinks.forEach(link => {
-      link.addEventListener('click', function() {
-        navLinks.forEach(navLink => navLink.classList.remove('active'));
-        this.classList.add('active');
-      });
+        link.addEventListener('click', function() {
+            navLinks.forEach(navLink => navLink.classList.remove('active'));
+            this.classList.add('active');
+        });
     });
-  });
-  
+
+    const hamburger = document.getElementById('hamburger');
+    const navMenu = document.getElementById('nav-menu');
+
+    hamburger.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+});
